c20cce725bcd3ec977ed33f9c2218e83fe140fc8
lilian <lillyannenjoki@gmail.com>
2022-12-03 00:43:19 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e) => {
+
+   e.preventDefault()
+   buildToDos(e.target)
+   form.reset()
+  })
+})
+
+
+ function buildToDos(form){
+   let toDo = form.new_task_description.value
+   let toDos = document.getElementById('tasks')
+   let li = document.createElement('li')
+   li.textContent = toDo
+   toDos.appendChild(li)
+ }
